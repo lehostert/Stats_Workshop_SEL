e780ca9a7b6fa37f2a56bab8bcce27f2e6915d24
Lauren E. Hostert <laurenehostert@gmail.com>
2020-05-11 22:30:13 -0500

    Refactor R script example to remove redundant code

--- databases_shiny_apps/connect-access-db.R
+++ databases_shiny_apps/connect-access-db.R
@@ -1,25 +1,6 @@
-library(tidyverse) #This package helps import spreadsheet-formatted data, primarily from Excel
-# library(data.table) #Helps with data management
-# library(dbplyr) #This package will help interface with databases by translating R code into database-specific variants
-# library(RODBC)
+library(tidyverse)
 library(odbc)
 library(DBI)
-# library(sqldf) # This package might help with more sophisticated SQL queries
-
-
-### with RODBC
-# connection_access <- odbcConnectAccess() ## only available for 32-bit version of Windows.
-# connection <- odbcConnect("<DSN>")
-# query <- "<SQL Query>"
-# data <- sqlQuery(connection, query, )
-# str(data)
-# odbcCloseAll() # At the end of an R session please remember to close the connection. 
-
-#ODBC -  Open Database Connectivity  a standard application programming interface (API) for accessing database management systems
-#DSN - File data sources store the connection information in a text file  not the Windos registry 
-
-
-# Finding your ODBC drivers https://support.office.com/en-us/article/administer-odbc-data-sources-b19f856b-5b9b-48c9-8b93-07484bfab5a7
 
 
 ### with odbc
@@ -27,7 +8,7 @@ odbcListDrivers() # to get a list of the drivers your computer knows about
 con <- dbConnect(odbc::odbc(), "2019_CREP_Database")
 dbListTables(con) # To get the list of tables in the database
 
-odbc_result <- DBI::dbReadTable(con, "Fish_Abundance") #The dbReadTable() command will return the data table from the database as class: "data.frame"
+odbc_result <- DBI::dbReadTable(con, "Fish_Abundance") # The dbReadTable() command will return the data table from the database as class: "data.frame"
 result_dbi<- as_tibble(odbc_result) # Use as_tibble() turn the dataframe as a tibble dataframe to continue with queries using the dplyr syntax
 result_dplyr <- as_tibble(tbl(con, "Fish_Abundance")) # OR directly return the data table from the database as a tibble
 
@@ -58,7 +39,7 @@ fish_locations_sql_dplyr <- as_tibble(tbl(con, sql("SELECT DISTINCT Fish_Abundan
 # As a tibble you will be able to manipulate it using any 
 # TODO can you use sql() for manipulating queries??
 
-## Using dplyr and tidyverse syntax
+## Using dplyr and tidyverse syntax only
 fish_locations_dplyr <- as_tibble(tbl(con, "Fish_Abundance")) %>% 
   left_join(as_tibble(tbl(con, "Established_Locations")), by = c("PU_Gap_Code", "Reach_Name")) %>% 
   select(PU_Gap_Code, Reach_Name, Event_Date, Site_Type, Latitude, Longitude, Stream_Name) %>% 
@@ -68,6 +49,11 @@ fish_locations_dplyr <- as_tibble(tbl(con, "Fish_Abundance")) %>%
 # You can either pull tables out one by one and then join them or use the tbl() function to pull tables from the DB and join them in the same lines of code
 # NOTE that if you don't specify the "by" for the `join` dplyr will automatically use all of the fields that are the same between the two tables. 
 # In this case removing the "by" argument would result in the same dataframe. 
+
+# NOTE: fish_locations_sql_dbi, fish_locations_sql_dplyr, and fish_locations_dplyr all have the same information (ie. number of observations and variables) 
+# but they are  different classes. 
+# The DBI query is just a class "data.frame" but the dplyr queries have explicitly been turned into class "tbl_df" "tbl" "data.frame"
+
 
 #### When you are done close your database connection ####
 dbDisconnect(con)
