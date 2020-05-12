d08c746c225973e4975047a9bab0f8a6b09a43ef
Lauren E. Hostert <laurenehostert@gmail.com>
2020-05-11 20:25:56 -0500

    Refactor example and documentation for connecting R to Access DB

--- databases_shiny_apps/README.md
+++ databases_shiny_apps/README.md
@@ -17,40 +17,74 @@
 1. Click **Start**, and then click **Control Panel**  
   (You may need to search for **Control Panel** in the Windows Start Menu)
 2. In the **Control Panel**, click **Administrative Tools**
-3. In the **Administrative Tools** windows select **ODBC Data Sources (64-bit)** unless you want to make a 32-bit driver. This will be dependant on which version (32-bit or 64-bit) you are running of Access.
+3. In the **Administrative Tools** windows select **ODBC Data Sources (64-bit)** unless you want to make a 32-bit driver. This will be dependent on which version of Access you are running (32-bit or 64-bit).
 
 ![ODBC Data Sources](images/Add_ODBC_data_source.PNG)
-4. For adding a database is the local to your computer select the **User DSN** tab.
-5. Click **Add**
+4. For adding a database is the local to your computer select the **User DSN** tab and Click **Add**
 
 ![Add New Data Source](images/Inked_Add_ODBC_data_source_2.jpg)
 
-6. Select the "Microsoft Access Driver (\*.mdb, \*.accdb)" driver type to use, and then click **Finish**.
+6. Select the _"Microsoft Access Driver (\*.mdb, \*.accdb)"_ driver type to use, and then click **Finish**.
 
 ![Select Data Source Type](images/Add_ODBC_data_source_3.PNG)
 
-7. Follow the instructions and enter the "Data Source Name" and a short Description of the database and click the "Select" button to navigate to the location of your database on your local machine.
+7. Follow the instructions and enter the "Data Source Name" and a short description of the database and click the "Select" button to navigate to the location of your database on your local machine.
 
 ![MS Access Data Setup](images/ODBC_MS_Access_Setup.PNG)
 ![MS Access Database location](images/ODBC_MS_Access_Setup_File_Navigation.PNG)
 
 8. Click **OK** in the **Select Database** window then **OK** in the **ODBC Microsoft Access Setup** window
 
-9. You should now see your newly created database driver in the list of available  User Data Sources
+9. You should now see your newly created database driver in the list of available __User Data Sources__
 
 ![MS Access Data Setup Success](images/ODBC_MS_Access_Driver_Setup.PNG)
 
 ### Install and load packages in R
-- `library(ODBC)`
-    - This is the library that will establish a connection between your
-- `library(DBI)`
-    - Once connected with the database this library will allow you to put things out of the database
+`library(ODBC)`
+    - This is the library that will establish a connection between your Access DB and your R session  
+`library(DBI)`
+    - Once connected with the database this library will allow you to put things out of the database  
+`library(tidyverse)`
+        - This will load the base  packages from the tidyverse including dplyr which can help you manipulate the data tables and run SQL queries
 
 ### Connect to the database, Run your analysis, and Close the connection
 
 ```R
-connection <- dbConnect(odbc::odbc(), "My_Database")
-odbc_result <- DBI::dbReadTable(connection, "Fish_Abundance")
-as_tibble(odbc_result)` #You can now use  the result as a typical tibble
-odbcCloseAll()`   #When you are finished be sure to sever the connection to the database
+connection <- DBI::dbConnect(odbc::odbc(), "My_Database")
+dbListTables(connection) # Get the list of tables in the database that is now connected
+odbc_result <- DBI::dbReadTable(connection, "Fish_Abundance") # Pull out specific tables from DB
+as_tibble(odbc_result)` # Turn those tables into tibbles to use with dplyr functions
 ```
+### Conducting queries
+
+#### Using DBI and SQL
+
+```R
+fish_locations_sql_dbi <- DBI::dbGetQuery(connection, "SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date,
+                                          Established_Locations.Site_Type, Established_Locations.Latitude,
+                                          Established_Locations.Longitude, Established_Locations.Stream_Name
+                                       FROM Fish_Abundance LEFT JOIN Established_Locations
+                                       ON (Fish_Abundance.PU_Gap_Code = Established_Locations.PU_Gap_Code)
+                                       AND (Fish_Abundance.Reach_Name = Established_Locations.Reach_Name)")
+```
+#### Using dplyr and SQL
+
+```R
+fish_locations_sql_dplyr <- as_tibble(tbl(connection, sql("SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date,
+                                          Established_Locations.Site_Type, Established_Locations.Latitude,
+                                          Established_Locations.Longitude, Established_Locations.Stream_Name
+                                       FROM Fish_Abundance LEFT JOIN Established_Locations
+                                       ON (Fish_Abundance.PU_Gap_Code = Established_Locations.PU_Gap_Code)
+                                       AND (Fish_Abundance.Reach_Name = Established_Locations.Reach_Name)")))
+```
+#### Using dplyr
+
+```R
+fish_locations_tidyverse <- as_tibble(tbl(connection, "Fish_Abundance")) %>%
+  left_join(as_tibble(tbl(connection, "Established_Locations")), by = c("PU_Gap_Code", "Reach_Name")) %>%
+  select(PU_Gap_Code, Reach_Name, Event_Date, Site_Type, Latitude, Longitude, Stream_Name) %>%
+  distinct()
+```
+### Close the connection
+ Finally, when you are finished interacting with the database close the connection.  
+ `dbDisconnect(connection)`
--- databases_shiny_apps/connect-access-db.R
+++ databases_shiny_apps/connect-access-db.R
@@ -1,19 +1,19 @@
 library(tidyverse) #This package helps import spreadsheet-formatted data, primarily from Excel
-library(data.table) #Helps with data management
-library(dbplyr) #This package will help interface with databases by translating R code into database-specific variants
-library(RODBC)
+# library(data.table) #Helps with data management
+# library(dbplyr) #This package will help interface with databases by translating R code into database-specific variants
+# library(RODBC)
 library(odbc)
 library(DBI)
 # library(sqldf) # This package might help with more sophisticated SQL queries
 
 
 ### with RODBC
-connection_access <- odbcConnectAccess() ## only available for 32-bit version of Windows.
-connection <- odbcConnect("<DSN>")
-query <- "<SQL Query>"
-data <- sqlQuery(connection, query, )
-str(data)
-odbcCloseAll() # At the end of an R session please remember to close the connection. 
+# connection_access <- odbcConnectAccess() ## only available for 32-bit version of Windows.
+# connection <- odbcConnect("<DSN>")
+# query <- "<SQL Query>"
+# data <- sqlQuery(connection, query, )
+# str(data)
+# odbcCloseAll() # At the end of an R session please remember to close the connection. 
 
 #ODBC -  Open Database Connectivity  a standard application programming interface (API) for accessing database management systems
 #DSN - File data sources store the connection information in a text file  not the Windos registry 
@@ -28,25 +28,24 @@ con <- dbConnect(odbc::odbc(), "2019_CREP_Database")
 dbListTables(con) # To get the list of tables in the database
 
 odbc_result <- DBI::dbReadTable(con, "Fish_Abundance") #The dbReadTable() command will return the data table from the database as class: "data.frame"
-result_dbi<- as_tibble(odbc_result) # Use as_tibble() turn the dataframe as a tibble to continue with queries using the dplyr syntax
-
+result_dbi<- as_tibble(odbc_result) # Use as_tibble() turn the dataframe as a tibble dataframe to continue with queries using the dplyr syntax
 result_dplyr <- as_tibble(tbl(con, "Fish_Abundance")) # OR directly return the data table from the database as a tibble
 
 #### Conducting Queries ####
 ## Using DBI and SQL 
-abc <- DBI::dbGetQuery(con, "SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date, 
+fish_locations_sql_dbi <- DBI::dbGetQuery(con, "SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date, 
                                           Established_Locations.Site_Type, Established_Locations.Latitude, 
                                           Established_Locations.Longitude, Established_Locations.Stream_Name 
                                        FROM Fish_Abundance LEFT JOIN Established_Locations 
                                        ON (Fish_Abundance.PU_Gap_Code = Established_Locations.PU_Gap_Code) 
                                        AND (Fish_Abundance.Reach_Name = Established_Locations.Reach_Name)") 
 
-# You can use this for SELECT queries only not queries that involve amnipulation. 
+# You can use dbGetQuery() for SELECT queries only not queries that involve manipulation. 
 
 
 
 ## Using dplyr and SQL 
-fish_locations_sql <- as_tibble(tbl(con, sql("SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date, 
+fish_locations_sql_dplyr <- as_tibble(tbl(con, sql("SELECT DISTINCT Fish_Abundance.PU_Gap_Code, Fish_Abundance.Reach_Name, Fish_Abundance.Event_Date, 
                                           Established_Locations.Site_Type, Established_Locations.Latitude, 
                                           Established_Locations.Longitude, Established_Locations.Stream_Name 
                                        FROM Fish_Abundance LEFT JOIN Established_Locations 
@@ -54,18 +53,22 @@ fish_locations_sql <- as_tibble(tbl(con, sql("SELECT DISTINCT Fish_Abundance.PU_
                                        AND (Fish_Abundance.Reach_Name = Established_Locations.Reach_Name)")))
 # tbl() this is a generic method for creating a table from a datasource. In this case the source is our database connection called "con"
 # sql() allows you to write SQL queries directly
-# as_tibble() qill turn the resulting object into a tibble/data.frame
+# as_tibble() will turn the resulting object into a tibble data.frame
+# If you remove the as_tibble() function you will get a result that is class ["tbl_ACCESS" "tbl_dbi"    "tbl_sql"    "tbl_lazy"   "tbl"]
+# As a tibble you will be able to manipulate it using any 
+# TODO can you use sql() for manipulating queries??
 
-# Using dplyr and tidyverse syntax
-fish_locations_tidyverse <- as_tibble(tbl(con, "Fish_Abundance")) %>% 
+## Using dplyr and tidyverse syntax
+fish_locations_dplyr <- as_tibble(tbl(con, "Fish_Abundance")) %>% 
   left_join(as_tibble(tbl(con, "Established_Locations")), by = c("PU_Gap_Code", "Reach_Name")) %>% 
   select(PU_Gap_Code, Reach_Name, Event_Date, Site_Type, Latitude, Longitude, Stream_Name) %>% 
   distinct()
 
 
-## NOTE that if you don't specify the "by" for the join dplyr will automatically use all of the fields that are the same between the two tables. 
+# You can either pull tables out one by one and then join them or use the tbl() function to pull tables from the DB and join them in the same lines of code
+# NOTE that if you don't specify the "by" for the `join` dplyr will automatically use all of the fields that are the same between the two tables. 
+# In this case removing the "by" argument would result in the same dataframe. 
 
-#### Close your database connection ####
-odbcCloseAll()
-dbDisconnect()
+#### When you are done close your database connection ####
+dbDisconnect(con)
 
